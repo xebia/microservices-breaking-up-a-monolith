52c6b8ade3df02c6cd084fec7dffa9e0d1d465f3
MvdLinden <mvanderlinden@xebia.com>
2015-09-05 17:51:58 +0200

    Copied from micro-a/src/ex1 repo

---
+++ src/exercise-circuitbreaker/catalog/src/main/java/com/xebia/catalog/rest/ProductResourceAssembler.java
@@ -0,0 +1,30 @@
+package com.xebia.catalog.rest;
+
+import com.xebia.catalog.domain.Product;
+import org.springframework.beans.BeanUtils;
+import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductResourceAssembler extends ResourceAssemblerSupport<Product, ProductResource> {
+
+    public ProductResourceAssembler() {
+        super(ProductController.class, ProductResource.class);
+    }
+
+    @Override
+    public ProductResource toResource(Product product) {
+        ProductResource resource = createResourceWithId(product.getUuid(), product);
+        BeanUtils.copyProperties(product, resource);
+        return resource;
+    }
+
+    public List<ProductResource> toResource(List<Product> products) {
+        List<ProductResource> result = new ArrayList<ProductResource>();
+        for (Product product:products) {
+            result.add(toResource(product));
+        }
+        return result;
+    }
+}
---
+++ src/exercise-circuitbreaker/catalog/src/test/java/com/xebia/catalog/rest/TestBase.java
@@ -0,0 +1,61 @@
+package com.xebia.catalog.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+import static org.junit.Assert.fail;
+
+public class TestBase {
+    @Autowired
+    protected WebApplicationContext webApplicationContext;
+    protected MediaType jsonContentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+    protected MediaType textType = new MediaType(MediaType.TEXT_PLAIN.getType());
+    protected MockMvc mockMvc;
+    protected HttpMessageConverter mappingJackson2HttpMessageConverter;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    protected String getTimeStamp() {
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+        Date cutoffDate = new Date();
+        return sdf.format(cutoffDate);
+    }
+
+    protected void waitAWhile(long intervalInMs) {
+        try {
+            Thread.sleep(intervalInMs);
+        } catch (Exception e) {
+            fail("Exception in testModifiedSinceReturnsOnlyNewPayments");
+        }
+    }
+
+    protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream().filter(
+                hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().get();
+
+        org.junit.Assert.assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
+}
