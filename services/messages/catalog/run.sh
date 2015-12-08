ad279a39ba5f3add8d6fb416c54d35ed8c303cc6
Jan Vermeir <jvermeir@xebia.com>
2015-12-08 21:02:16 +0100

    Created Docker version

--- services/messages/catalog/run.sh
+++ services/messages/catalog/run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-mvn exec:java -Dexec.mainClass="com.xebia.catalog.CatalogApplication"
+for i in $( docker ps -aq -f name=catalog)
+do
+    docker stop $i
+done
 
+for i in $( docker ps -aq -f status=exited)
+do
+    docker rm $i
+done
+
+docker run -d --name catalog -p 9004:9004 xebia/catalog_msg
+
+# To start as a normal Java program
+#mvn exec:java -Dexec.mainClass="com.xebia.catalog.CatalogApplication"
