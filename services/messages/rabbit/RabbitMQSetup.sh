92f4fad89d166ee6d304b1afb72eb3f654081873
Jan Vermeir <jvermeir@xebia.com>
2015-12-12 22:22:07 +0100

    Added root directory

--- services/messages/rabbit/RabbitMQSetup.sh
+++ services/messages/rabbit/RabbitMQSetup.sh
@@ -3,16 +3,17 @@
 # and rabbitmq-server has started
 # Run 'runRabbit.sh' to start RabbitMQ in a container
 RABBIT_MQ_HOST=192.168.99.100
+DIR=`dirname $0`
 
-./rabbitmqadmin -H $RABBIT_MQ_HOST declare exchange name="shop" type="topic"
-./rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="fulfillment.order"
-./rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="payment.order"
-./rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="shop.payment"
-./rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="fulfillment.payment"
-./rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="fulfillment.order" routing_key="orders"
-./rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="payment.order" routing_key="orders"
-./rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="shop.payment" routing_key="payments"
-./rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="fulfillment.payment" routing_key="payments"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare exchange name="shop" type="topic"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="fulfillment.order"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="payment.order"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="shop.payment"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="fulfillment.payment"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="fulfillment.order" routing_key="orders"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="payment.order" routing_key="orders"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="shop.payment" routing_key="payments"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="fulfillment.payment" routing_key="payments"
 
 # routing key and source are used as parameter in RabbitMQ calls. See e.g. OrderController.createNewOrder in the shop project.
 # destination refers to a queue (see above).
