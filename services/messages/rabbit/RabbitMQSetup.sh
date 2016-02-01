584ea1d0832fe2e634422f8473c7658703207d64
Jan Vermeir <jvermeir@xebia.com>
2016-02-01 20:36:58 +0100

    New queues and bindings

--- services/messages/rabbit/RabbitMQSetup.sh
+++ services/messages/rabbit/RabbitMQSetup.sh
@@ -5,17 +5,23 @@
 RABBIT_MQ_HOST=192.168.99.100
 DIR=`dirname $0`
 
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST delete exchange name="shop"
 $DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare exchange name="shop" type="topic"
-$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="fulfillment.order"
-$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="payment.order"
-$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="shop.payment"
-$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="fulfillment.payment"
-$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="fulfillment.order" routing_key="orders"
-$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="payment.order" routing_key="orders"
-$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="shop.payment" routing_key="payments"
-$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="fulfillment.payment" routing_key="payments"
 
-# routing key and source are used as parameter in RabbitMQ calls. See e.g. OrderController.createNewOrder in the shop project.
-# destination refers to a queue (see above).
-# a destination has a listener @RabbitListener(queues = "fulfillment.payment")
-# see e.g. Eventlistener in the fulfillment project.
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="orderPaid"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="orderPaid" routing_key="orderPaid"
+
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="orderCompleted"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="orderCompleted" routing_key="orderCompleted"
+
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="orderShipped"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="orderShipped" routing_key="orderShipped"
+
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="handlePayment"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="handlePayment" routing_key="handlePayment"
+
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="startShopping"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="startShopping" routing_key="startShopping"
+
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare queue name="handleFulfillment"
+$DIR/rabbitmqadmin -H $RABBIT_MQ_HOST declare binding source="shop" destination="handleFulfillment" routing_key="handleFulfillment"
