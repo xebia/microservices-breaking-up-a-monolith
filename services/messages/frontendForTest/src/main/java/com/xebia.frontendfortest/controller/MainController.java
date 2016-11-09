f8375e218699f34874cb479a1e058a0c8e3bd70e
MvdLinden <mvanderlinden@xebia.com>
2016-11-09 21:57:22 +0100

    Added CORS and token handling

--- services/messages/frontendForTest/src/main/java/com/xebia.frontendfortest/controller/MainController.java
+++ services/messages/frontendForTest/src/main/java/com/xebia.frontendfortest/controller/MainController.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,8 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@EnableRedisHttpSession
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class MainController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -24,8 +29,9 @@ public class MainController {
     }
 
     @RequestMapping("/fulfillment/list")
-    @CrossOrigin(origins = "http://localhost:8082", maxAge = 3600,
-        allowedHeaders={"x-auth-token", "x-requested-with"})
+    @CrossOrigin(origins = "*", maxAge = 3600)
+//    @CrossOrigin(origins = "http://localhost:8082", maxAge = 3600,
+//        allowedHeaders={"x-auth-token", "x-requested-with"})
     @ResponseBody
     String listOfFulfillments() {
         logger.info(">>>> 8083 - Fulfillment - List method");
