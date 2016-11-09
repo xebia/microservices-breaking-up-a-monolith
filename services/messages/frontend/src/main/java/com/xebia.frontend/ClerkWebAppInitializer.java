ac783612bc501134fa3cef485b2474ff8e877603
MvdLinden <mvanderlinden@xebia.com>
2016-11-09 21:52:42 +0100

    Added CSRF and token handling

--- services/messages/frontend/src/main/java/com/xebia.frontend/ClerkWebAppInitializer.java
+++ services/messages/frontend/src/main/java/com/xebia.frontend/ClerkWebAppInitializer.java
@@ -11,15 +11,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.security.Principal;
 import java.util.Collections;
 import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.http.Cookie;
 
 
 @Configuration
@@ -35,16 +45,44 @@ public class ClerkWebAppInitializer {
   protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-      http
-          .httpBasic()
-          .and()
-          .authorizeRequests()
-          .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
-          .anyRequest().authenticated().and()
-          .csrf()
-          .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+      http.httpBasic().and().authorizeRequests()
+              .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
+              .authenticated().and().csrf()
+              .csrfTokenRepository(csrfTokenRepository()).and()
+              .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+    }
+
+    private Filter csrfHeaderFilter() {
+      return new OncePerRequestFilter() {
+        @Override
+        protected void doFilterInternal(HttpServletRequest request,
+                                        HttpServletResponse response, FilterChain filterChain)
+                throws ServletException, IOException {
+          CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+                  .getName());
+          if (csrf != null) {
+            Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+            String token = csrf.getToken();
+            if (cookie == null || token != null
+                    && !token.equals(cookie.getValue())) {
+              cookie = new Cookie("XSRF-TOKEN", token);
+              cookie.setPath("/");
+              response.addCookie(cookie);
+            }
+          }
+          filterChain.doFilter(request, response);
+        }
+      };
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+      HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+      repository.setHeaderName("X-XSRF-TOKEN");
+      return repository;
     }
   }
+
+
 
   @RequestMapping("/user")
   public Principal user(Principal user) {
--- services/messages/frontend/src/main/resources/static/js/services.js
+++ services/messages/frontend/src/main/resources/static/js/services.js
@@ -2,15 +2,27 @@ app.service("Clerks", function($resource) {
     return $resource("http://localhost:8082/clerk/list");
 })
 
-app.service("Fulfillments", function($resource, $http) {
-    return $resource($http.get('token')
-    .then(function(response) {
-            $http({
-              url : 'http://localhost:8083/fulfillment/list',
-              method : 'GET',
-              headers : {
-                'X-Auth-Token' : response.data.token
-              }
-            });
-        }));
+app.service("Fulfillments", function($http) {
+    $http.get('token').success(function(token) {
+		$http({
+			url : 'http://localhost:8083/fulfillment/list',
+			method : 'GET',
+			headers : {
+				'X-Auth-Token' : token.token
+			}
+		}).success(function(data) {
+           return data
+		});
+	})
+
+//    return $resource($http.get('token')
+//    .then(function(response) {
+//            $http({
+//              url : 'http://localhost:8083/fulfillment/list',
+//              method : 'GET',
+//              headers : {
+//                'X-Auth-Token' : response.data.token
+//              }
+//            });
+//        }));
 })
