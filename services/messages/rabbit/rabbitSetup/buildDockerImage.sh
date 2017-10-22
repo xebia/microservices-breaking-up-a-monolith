0ebc231b34478ab860728e38a34f4483f4fe3a36
Jan Vermeir <jvermeir@hotmail.com>
2017-10-22 16:53:24 +0200

    Removed cleanup in buildDockerImages.sh

--- services/messages/rabbit/rabbitSetup/buildDockerImage.sh
+++ services/messages/rabbit/rabbitSetup/buildDockerImage.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Remove first so container will be rebuild even if only the setup script changes
-docker rmi -f xebia/rabbitmq_msg_setup
-
 docker build -t xebia/rabbitmq_msg_setup .
